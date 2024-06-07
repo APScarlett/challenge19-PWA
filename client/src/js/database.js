46868d7254848d041961b4c364d5a575b8b38a56
Alexander Scarlett <ascarlett92@yahoo.com>
2024-06-07 01:47:33 -0400

    updated database.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -14,7 +14,7 @@ const initdb = async () =>
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
-  console.log('put to the database');
+  console.log('PUT to the database');
   const jateDb = await openDB('jate', 'readwrite');
   const store =tx.objectStore('jate');
   const request = store.put({id: 1, jate: content});
@@ -23,7 +23,14 @@ export const putDb = async (content) => {
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
-  console.log
-}
+  console.log('GET all from the database');
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  const request = store.getALL();
+  const result = await request;
+  console.log('result.value, result');
+  return result;
+};
 
 initdb();
