fc128af7c320722580bcdaebf0bd15904722dc77
Alexander Scarlett <ascarlett92@yahoo.com>
2024-06-07 01:20:55 -0400

    updated database.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,17 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  console.log('put to the database');
+  const jateDb = await openDB('jate', 'readwrite');
+  const store =tx.objectStore('jate');
+  const request = store.put({id: 1, jate: content});
+  console.log('Data saved to the database', result);
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  console.log
+}
 
 initdb();
